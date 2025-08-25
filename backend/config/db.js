fbe3e21f2433bd604cde3aa108f12a4192bd5be3
sanjay0224 <sanjaykumarna2004@gmail.com>
2025-08-25 21:17:04 +0530

    cloud db connected

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/eshop";
+    const uri = process.env.MONGO_URI || "mongodb+srv://adminUser:adminUser@cluster0.bzh7gi1.mongodb.net";
     await mongoose.connect(uri); // modern driver: no extra options needed
     console.log("✅ MongoDB connected");
   } catch (err) {
